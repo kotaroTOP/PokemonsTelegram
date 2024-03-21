c7c72fb5829ec8668442af6b99e783a46acc90a8
KotaroTOP <mugosikatomurske@gmail.com>
2024-03-21 19:52:16 +0300

    фиксация прошла успешно)

---
+++ api.py
@@ -0,0 +1,2 @@
+ApiKey = 'live_fyKKfIp6T7r113TGg2A01bk53h9Vs1bHNUJQ8XIwUheC0ZM8tCPjWcscDzvELXrL' # токен API
+#HEH!"
---
+++ config.py
@@ -0,0 +1,2 @@
+token = "7079625634:AAH7Iq9slcjeIeqQLIqtyH3APRX-YmbyUsc" # токен для бота
+#HEH!"
